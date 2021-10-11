78eff3fa2af15d9815054c9b25fe6834022de1a5
Ксения Суина <KDSuina@stud.kpfu.ru>
2021-10-12 00:35:39 +0300

    Добавьте файлы проекта.

---
+++ zadanie2_2/zadanie2_2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std; 
+int fib(int n) {
+    unsigned long int fn, f1 = 0, f2 = 1; 
+    if (n == 1) {
+        return f1;
+    }
+    if (n == 2) {
+        return f2;
+    }
+    for (int i = 2; i < n; i += 1) {
+        fn = f1 + f2;
+        f1 = f2; 
+        f2 = fn; 
+    }
+    return fn;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите натуральное число n ";
+    cin >> n;
+    cout << fib(n) << endl;
+}
